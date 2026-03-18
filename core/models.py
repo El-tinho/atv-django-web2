2e51592b888d81088bde768e19c1978e7852e0af
Elton Santos <eess4@discente.ifpe.edu.br>
2026-03-18 03:16:22 -0300

    Inicialização do projeto Django e configuração do ambiente

---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Tag(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
