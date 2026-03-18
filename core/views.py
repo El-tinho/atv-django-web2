ce8f802b3b66f987c80aac21ee81a41fdeb9ce04
Elton Santos <eess4@discente.ifpe.edu.br>
2026-03-18 04:18:41 -0300

    Implementadas views, urls e templates do blog

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def lista_posts(request):
+    posts = Post.objects.all()
+    return render(request, 'core/lista_posts.html', {'posts': posts})
+
+def detalhe_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'core/detalhe_post.html', {'post': post})
